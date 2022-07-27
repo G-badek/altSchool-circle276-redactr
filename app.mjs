7f6f63aff4897a5a63a6e736f2dea74eaf6914df
Akinjibade Fortunate <68448145+G-badek@users.noreply.github.com>
2022-07-27 02:53:59 +0000

    Total Functionality Completed

--- app.mjs
+++ app.mjs
@@ -18,14 +18,14 @@ function startApp() {
   const returnBtn = document.querySelector('#return-btn');
   const clearBtn = document.querySelector('#clear-btn');
   const copyBtn = document.querySelector('#copy-btn');
-  const Scanned = document.querySelector('#scanned');
+  const shareBtn = document.querySelector('#share-btn');
+  const scanned = document.querySelector('#scanned');
   const matched = document.querySelector('#matched');
-  const scrambled = document.querySelector('#scrambled'); 
+  const scrambled = document.querySelector('#scrambled');
   const timed = document.querySelector('#timed');
-  
-  let main, redact, returnText;
-  
-  sign.value = '*';
+
+  let main, redact, returnText, dont;
+
 
   /////////////////////////////////////////////
 
@@ -37,21 +37,34 @@ function startApp() {
   //REDACT BUTT0N
 
   redactBtn.addEventListener("click", (e) => {
+    const startTime = Date.now();
+    if (returnText) {
+      returnBtn.click();
+    }
     let main = mainTexts.value.split(" ");
     let redact = redactTexts.value.split(" ");
+
     if (!returnText) {
       returnText = mainTexts.value.split(" ");
     }
-    redacting(main, redact);
+
+    let [scan, match, scramble] = redacting(main, redact);
     setTimeout(() => {
       mainTexts.value = main.join(" ");
-    }, 300);
+      const endTime = Date.now();
+      let timeCompleted = `${endTime - startTime}ms`;
+      stats(scan, match, scramble, timeCompleted);
+    }, 10);
   });
 
   //RETURN BUTTON
 
   returnBtn.addEventListener("click", (e) => {
-    returnTexts(returnText);
+    if (returnText) {
+      returnTexts(returnText);
+      returnText = "";
+    }
+
   });
 
   //CLEAR BUTTON
@@ -59,28 +72,78 @@ function startApp() {
   clearBtn.addEventListener("click", (e) => {
     mainTexts.value = "";
     redactTexts.value = "";
-    sign.value = "*";
+    returnText = "";
+    sign.value = "";
+    scanned.innerText = `Scanned word(s) : ${0}`;
+    matched.innerText = `Matched word(s) : ${0}`;
+    scrambled.innerText = `Scrambled character(s) : ${0}`;
+    timed.innerText = `Execution time : ${0}`;
   });
 
   //COPY BUTTON
 
   copyBtn.addEventListener("click", (e) => {
+    dont = false;
     mainTexts.select();
     mainTexts.setSelectionRange(0, 99999);
     copyBtn.innerText = "Copied";
+
+    navigator.clipboard.writeText(mainTexts.value).then(function() {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+
     setTimeout(() => {
       copyBtn.innerText = "Copy";
     }, 3000);
-    navigator.clipboard.writeText(mainTexts.value).then(function() {
-        console.log('Async: Copying to clipboard was successful!');
-      }, function(err) {
-        console.error('Async: Could not copy text: ', err);
-    });
+    setTimeout(() => {
+      dont = true;
+      mainTexts.blur();
+    }, 500);
+
+  });
+
+  // SHARE BUTTON
+
+  shareBtn.addEventListener("click", async (e) => {
+    let data = {
+      title: "",
+      text: `${mainTexts.value}`
+    }
+    try {
+      await navigator.share(data);
+    } catch (err) {
+      console.log(err);
+    }
+
   });
 
   //SIGN INPUT
 
   sign.addEventListener('change', (e) => {
+    returnBtn.click();
+  });
+
+  //MAIN INPUT
+
+  mainTexts.addEventListener('focus', (e) => {
+    // mainTexts.select();
+    // mainTexts.setSelectionRange(0, 99999);
+    if (dont) {
+      returnBtn.click();
+    }
+  });
+
+  mainTexts.addEventListener('keyup', (e) => {
+  });
+
+  // });
+
+
+  //REDACT INPUT
+
+  redactTexts.addEventListener('keydown', (e) => {
     returnBtn.click();
   });
 
@@ -95,23 +158,40 @@ function startApp() {
   //REDACTING FUNCTION
 
   const redacting = (main, redact) => {
+    let scanned = main.length, matched = 0, scrambled = 0;
     for (let i = 0; i < redact.length; i++) {
       for (let j = 0; j < main.length; j++) {
-        if (redact[i] == main[j]) {
+        if (redact[i].toUpperCase() == main[j].toUpperCase()) {
           let count = main[j].length;
+          matched += 1;
+          scrambled += count;
           main[j] = "";
           for (let v = 0; v < count; v++) {
-            main[j] += sign.value;
+            main[j] += (sign.value || "*");
           }
         }
       }
     }
+    return ([scanned, matched, scrambled])
   }
 
   //RETURN FUNCTION
 
   const returnTexts = (texts) => {
     mainTexts.value = texts.join(" ");
+    scanned.innerText = `Scanned word(s) : ${0}`;
+    matched.innerText = `Matched word(s) : ${0}`;
+    timed.innerText = `Execution time : ${0}`;
+    scrambled.innerText = `Scrambled character(s) : ${0}`;
+  }
+
+  //STATS FUNCTION
+
+  const stats = (scan, match, scramble, timeCompleted) => {
+    scanned.innerText = `Scanned word(s) : ${scan}`;
+    matched.innerText = `Matched word(s) : ${match}`;
+    scrambled.innerText = `Scrambled character(s) : ${scramble}`;
+    timed.innerText = `Execution time : ${timeCompleted}`;
   }
 
   /////////////////////////////////////////////
